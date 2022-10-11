10e22e5e7b52c82d4f00b4606b2a584299f4cf96
Ben <baziz10sa@gmail.com>
2022-10-11 12:58:55 -0700

    calendar

---
+++ index.js
@@ -0,0 +1,20 @@
+const monthEl = document.querySelector(".date h1");
+
+const monthInx = new Date().getMonth();
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+monthEl.innerText = month[monthInx];
